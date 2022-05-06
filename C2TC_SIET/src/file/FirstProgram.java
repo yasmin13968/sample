1ed5a22b8d46c6064659ed680958ae7fa38bd204
Gayatri Vastrad <Gayatri Vastrad@LAPTOP-H2HHKS0N>
2022-05-06 18:27:07 +0530

    first commit

---
+++ C2TC_SIET/src/file/FirstProgram.java
@@ -0,0 +1,10 @@
+package file;
+public class FirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		
+    System.out.println("Welcome to java");
+	}
+
+}
---
+++ C2TC_SIET/src/refactor/SecondProgram.java
@@ -0,0 +1,9 @@
+package refactor;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
